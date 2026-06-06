adf658ac85d0c170c0e9106f063b6edafa81dde1
Marieme Dieng <marieme.dieng2@etu.ussein.edu.sn>
2026-06-06 21:03:24 +0000

    Connect frontend to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://agrosmart-backend-ej4n.onrender.com/api'
 })
 
-// Ajoute le token automatiquement
 API.interceptors.request.use(req => {
   const token = localStorage.getItem('token')
   if (token) req.headers.Authorization = `Bearer ${token}`
